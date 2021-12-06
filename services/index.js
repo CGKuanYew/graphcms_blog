32c55eb0415999121a93002fb2f77fdd041ef4cc
Conrad Goh <88553340+CGKuanYew@users.noreply.github.com>
2021-12-06 17:22:24 +0800

    Update index.js

--- services/index.js
+++ services/index.js
@@ -188,3 +188,47 @@ export const getFeaturedPosts = async () => {
 
   return result.posts;
 };
+
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: {categories_some: {slug: $slug}}) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
+
+
+
+
+
+
